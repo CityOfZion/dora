7aa5b6c21a9df36da0bdbf520bc37b17dae8c597
Maxwell Lasky <mlasky46@gmail.com>
2020-06-26 09:56:31 -0600

    Implements redux logic and routing in Search component

---
+++ src/actions/searchActions.ts
@@ -0,0 +1,128 @@
+import { isEmpty } from 'lodash-es'
+import { ThunkDispatch } from 'redux-thunk'
+import { Dispatch, Action } from 'redux'
+
+import { GENERATE_BASE_URL, SEARCH_TYPES } from '../constants'
+import { State } from '../reducers/searchReducer'
+import { Balance } from '../reducers/addressReducer'
+
+export const SEARCH_INPUT_ENTERED = 'SEARCH_INPUT_ENTERED'
+export const searchInputEntered = (search: string) => (
+  dispatch: Dispatch,
+): void => {
+  dispatch({
+    type: SEARCH_INPUT_ENTERED,
+    search,
+  })
+}
+
+export const SEARCH_INPUT_ENTERED_SUCCESS = 'SEARCH_INPUT_ENTERED_SUCCESS'
+export const searchInputEnteredSuccess = (
+  search: string,
+  searchType: string,
+) => (dispatch: Dispatch): void => {
+  dispatch({
+    type: SEARCH_INPUT_ENTERED_SUCCESS,
+    searchType,
+    search,
+    receivedAt: Date.now(),
+  })
+}
+
+export const SEARCH_INPUT_ENTERED_ERROR = 'SEARCH_INPUT_ENTERED_ERROR'
+export const searchInputEnteredError = (errorMessage: string) => (
+  dispatch: Dispatch,
+): void => {
+  dispatch({
+    type: SEARCH_INPUT_ENTERED_ERROR,
+    errorMessage,
+    receivedAt: Date.now(),
+  })
+}
+
+export const CLEAR_SEARCH_INPUT_STATE = 'CLEAR_SEARCH_INPUT'
+export const clearSearchInputState = () => (dispatch: Dispatch): void => {
+  dispatch({
+    type: CLEAR_SEARCH_INPUT_STATE,
+  })
+}
+
+export const CLEAR_SEARCH_INPUT_ENTERED_ERROR =
+  'CLEAR_SEARCH_INPUT_ENTERED_ERROR'
+export const clearSearchInputError = () => (dispatch: Dispatch): void => {
+  dispatch({
+    type: CLEAR_SEARCH_INPUT_ENTERED_ERROR,
+  })
+}
+
+export function handleSearchInput(rawSearch: string) {
+  return async (
+    dispatch: ThunkDispatch<State, void, Action>,
+  ): Promise<void> => {
+    const search = rawSearch.replace(',', '')
+    dispatch(searchInputEntered(search))
+
+    try {
+      const searchType = await determineSearchType(search)
+      // TODO: returning the json here would prevent duplicate requests
+      // but will introduce added complexity
+      if (searchType) {
+        dispatch(searchInputEnteredSuccess(search, searchType))
+        return dispatch(clearSearchInputState())
+      }
+
+      return dispatch(searchInputEnteredError('No results found.'))
+    } catch (e) {
+      dispatch(searchInputEnteredError(e.message))
+    }
+  }
+}
+
+// TODO: refactor for performance optimization - we should
+// dispatch the appropriate success action with the appropriate JSON payload
+export async function determineSearchType(search: string): Promise<string> {
+  const isPossibleTxOrContract = search.includes('0x')
+
+  const invokePromiseAndIgnoreError = (url: string): Promise<{}> =>
+    fetch(url)
+      .then(result => result && result.json())
+      .catch(e => undefined)
+
+  const urls = []
+
+  if (isPossibleTxOrContract) {
+    urls.push(
+      ...[
+        `${GENERATE_BASE_URL()}/get_transaction/${search}`,
+        `${GENERATE_BASE_URL()}/get_contract/${search}`,
+      ],
+    )
+  } else {
+    urls.push(
+      ...[
+        `${GENERATE_BASE_URL()}/get_balance/${search}`,
+        `${GENERATE_BASE_URL()}/get_block/${search}`,
+      ],
+    )
+  }
+
+  const results = await Promise.all(
+    urls
+      .map(url => invokePromiseAndIgnoreError.bind(null, url))
+      .map(req => req()),
+  )
+
+  if (isPossibleTxOrContract) {
+    const [transaction, contract] = results
+    if (!isEmpty(transaction)) return SEARCH_TYPES.TRANSACTION
+    if (contract) return SEARCH_TYPES.CONTRACT
+  } else {
+    const balance = results[0] as Balance[]
+    const block = results[1]
+
+    if (balance && balance.length) return SEARCH_TYPES.ADDRESS
+    if (block) return SEARCH_TYPES.BLOCK
+  }
+
+  return ''
+}
--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,16 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { ReactComponent as Magnify } from '../../assets/icons/magnify.svg'
 import './Search.scss'
-
+import { handleSearchInput } from '../../actions/searchActions'
+import { State as SearchState } from '../../reducers/searchReducer'
 import { useHistory } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { SEARCH_TYPES } from '../../constants'
 
 const Search: React.FC<{}> = () => {
+  const dispatch = useDispatch()
+  const history = useHistory()
+
+  const [searchValue, updateSearchValue] = useState('')
+  const searchState = useSelector(
+    ({ search }: { search: SearchState }) => search,
+  )
+  const { searchType } = searchState
+
+  useEffect(() => {
+    switch (searchType) {
+      case SEARCH_TYPES.TRANSACTION:
+        return history.push(`/transaction/${searchValue}`)
+      case SEARCH_TYPES.CONTRACT:
+        return history.push(`/contract/${searchValue}`)
+      case SEARCH_TYPES.ADDRESS:
+        return history.push(`/address/${searchValue}`)
+      case SEARCH_TYPES.BLOCK:
+        return history.push(`/block/${searchValue}`)
+      default:
+        break
+    }
+  }, [history, searchType, searchValue])
+
+  function handleSearch(e: React.SyntheticEvent): void {
+    e.preventDefault()
+    dispatch(handleSearchInput(searchValue))
+  }
+
+  function updateSearch(e: React.SyntheticEvent): void {
+    const target = e.target as HTMLInputElement
+    const searchTerms = target.value
+    updateSearchValue(searchTerms)
+  }
+
   return (
     <div id="Search">
-      <form>
-        <input placeholder="Search for Block Height, Hash, Address or transaction id"></input>{' '}
-        <Magnify />
+      <form onSubmit={handleSearch}>
+        <input
+          value={searchValue}
+          onChange={updateSearch}
+          placeholder="Search for Block Height, Hash, Address or transaction id"
+        ></input>{' '}
+        <Magnify onClick={handleSearch} />
       </form>
     </div>
   )
