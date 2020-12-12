3ae928afb7d3560aef725cca5d9a425b83855140
Maxwell Lasky <mlasky46@gmail.com>
2020-12-12 14:16:23 -0700

    test deploy trigger

--- src/components/navigation/Footer/Footer.tsx
+++ src/components/navigation/Footer/Footer.tsx
@@ -74,7 +74,7 @@ const Footer: React.FC = (): ReactElement => {
               href="https://coz.io"
               target="_blank"
               rel="noopener noreferrer"
-              data-test="test2"
+              data-test="test3"
             >
               COZ
             </a>
