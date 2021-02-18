541d745b5768fd5da92bbe17010465f5234b598e
Maxwell Lasky <mlasky46@gmail.com>
2021-02-18 09:24:43 -0700

    Updates PlatformCell.tsx

--- src/components/platform-cell/PlatformCell.tsx
+++ src/components/platform-cell/PlatformCell.tsx
@@ -2,9 +2,10 @@ import React, { ReactElement } from 'react'
 
 import Neo2 from '../../assets/icons/neo2.svg'
 import Neo3 from '../../assets/icons/neo3.svg'
+import './PlatformCell.scss'
 
 const PlatformCell = ({ chain }: { chain: string | void }): ReactElement => (
-  <div className="txid-index-cell">
+  <div className="txid-index-cell PlatformCell">
     {chain === 'neo2' ? (
       <div className="neo2-platform-cell">
         <img src={Neo2} alt="NEO 2" />
