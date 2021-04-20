a916e6514770bb3ddf860096a91a9fb462437de3
Kenedy Ribeiro <helplace2019@gmail.com>
2021-04-20 16:56:00 -0300

    Add the Blockchain Version dropdown to Monitor page #304

--- src/contexts/MonitorContext.tsx
+++ src/contexts/MonitorContext.tsx
@@ -1,10 +1,14 @@
 import React, { createContext, useState } from 'react'
 
+export type TFilterName = 'N3' | 'N2' | 'Default' | 'testnet' | 'mainnet'
+
 export type MonitorContent = {
   showMessage: boolean
   message: string
+  filterName: TFilterName
   setShowMessage: React.Dispatch<React.SetStateAction<boolean>>
   setMessage: React.Dispatch<React.SetStateAction<string>>
+  setFilterName: React.Dispatch<React.SetStateAction<TFilterName>>
 }
 
 export const MonitorContext = createContext({} as MonitorContent)
@@ -12,10 +16,18 @@ export const MonitorContext = createContext({} as MonitorContent)
 export const MonitorProvider: React.FC = ({ children }) => {
   const [showMessage, setShowMessage] = useState(false)
   const [message, setMessage] = useState('')
+  const [filterName, setFilterName] = useState<TFilterName>('N3')
 
   return (
     <MonitorContext.Provider
-      value={{ message, showMessage, setMessage, setShowMessage }}
+      value={{
+        message,
+        showMessage,
+        filterName,
+        setMessage,
+        setShowMessage,
+        setFilterName,
+      }}
     >
       {children}
     </MonitorContext.Provider>
