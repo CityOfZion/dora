1eec22b589dd257be26c67a3a29ab4be8394d6f4
lllwvlvwlll <lllwvlvwlll@gmail.com>
2021-08-04 09:43:50 -0600

    finishes shunt for N3 testnets

--- src/pages/monitor/Monitor.tsx
+++ src/pages/monitor/Monitor.tsx
@@ -335,6 +335,9 @@ const Monitor: React.FC<{}> = () => {
     } else {
       //temporary state, remove when api cuts over
       let mutableNetwork = network
+      if (protocol === 'neo3' && mutableNetwork === 'testnet') {
+        mutableNetwork = 'testnet_rc3'
+      }
       if (mutableNetwork === 'testnet_rc4') {
         mutableNetwork = 'testnet'
       }
