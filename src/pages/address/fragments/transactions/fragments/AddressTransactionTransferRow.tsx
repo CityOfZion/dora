1f90b4edcb4aca49145d460dbdafb411fda3aef2
Thacryba <thaiscrysbastos@gmail.com.br>
2024-03-01 17:39:59 -0300

    CU-86drpnnr5 - Fix issue where NEP-11 transfers are being shown as NEP-17 transfers

--- src/pages/address/fragments/transactions/fragments/AddressTransactionTransferRow.tsx
+++ src/pages/address/fragments/transactions/fragments/AddressTransactionTransferRow.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import { Transfer } from '../AddressTransaction'
+import { Transfer, Notification } from '../AddressTransaction'
 import tokens from '../../../../../assets/nep5/svg'
 import { truncateHash } from '../../../../../utils/formatter'
 import { TransactionAddressLink } from '../../../../../components/transaction/TransactionAddressLink'
 
 type Props = {
   transfers: Transfer[]
+  notifications: Notification[]
   chain: string
   network: string
 }
 
 const AddressTransactionTransfer: React.FC<Props> = ({
   transfers,
+  notifications,
   ...props
 }: Props) => {
   return (
@@ -26,31 +28,42 @@ const AddressTransactionTransfer: React.FC<Props> = ({
             <label>Type</label>
           </div>
 
-          {transfers.map(transfer => (
-            <div
-              className="address-transactions__table--transfers-values"
-              key={transfer.from + transfer.to + transfer.amount}
-            >
-              <TransactionAddressLink address={transfer.from} {...props} />
-              <TransactionAddressLink address={transfer.to} {...props} />
+          {transfers.map(transfer => {
+            const notification = notifications.find(
+              notification => notification.contract === transfer.scripthash,
+            )
+            const type =
+              notification?.state.length === 3
+                ? 'NEP-17 Transfer'
+                : notification?.state.length === 4
+                ? 'NEP-11 Transfer'
+                : 'Transfer'
+            return (
+              <div
+                className="address-transactions__table--transfers-values"
+                key={transfer.from + transfer.to + transfer.amount}
+              >
+                <TransactionAddressLink address={transfer.from} {...props} />
+                <TransactionAddressLink address={transfer.to} {...props} />
 
-              <span className="whitespace-no-wrap">
-                {truncateHash(transfer.symbol, true, 10, 4)}
-              </span>
-              <span className="whitespace-no-wrap">
-                {tokens[transfer.symbol ?? 'NEO'] && (
-                  <img
-                    width={15}
-                    height={10}
-                    src={tokens[transfer.symbol ?? 'NEO']}
-                    alt=""
-                  />
-                )}
-                {transfer.amount}
-              </span>
-              <span>NEP-17 Transfer</span>
-            </div>
-          ))}
+                <span className="whitespace-no-wrap">
+                  {truncateHash(transfer.symbol, true, 10, 4)}
+                </span>
+                <span className="whitespace-no-wrap">
+                  {tokens[transfer.symbol ?? 'NEO'] && (
+                    <img
+                      width={15}
+                      height={10}
+                      src={tokens[transfer.symbol ?? 'NEO']}
+                      alt=""
+                    />
+                  )}
+                  {transfer.amount}
+                </span>
+                <span>{type}</span>
+              </div>
+            )
+          })}
         </>
       )}
     </div>
