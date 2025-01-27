0f32948ebaeb7e85688ca899af57676df75e8998
Erik van den Brink <git@erikvandenbrink.com>
2025-01-27 14:27:12 +0100

    configurable API host, base path and websocket host

--- src/rest.ts
+++ src/rest.ts
@@ -1,14 +1,14 @@
 import { NeoRESTApi } from '@cityofzion/dora-ts/dist/api'
 
-console.log('WTF:', process.env.API_HOST)
-
 export const NeoRest = new NeoRESTApi({
   doraUrl:
-    process.env.API_HOST === undefined || process.env.API_HOST === ''
+    process.env.REACT_APP_API_HOST === undefined ||
+    process.env.REACT_APP_API_HOST === ''
       ? 'https://dora.coz.io'
-      : process.env.API_HOST,
+      : process.env.REACT_APP_API_HOST,
   endpoint:
-    process.env.API_BASE_PATH === undefined || process.env.API_BASE_PATH === ''
+    process.env.REACT_APP_API_BASE_PATH === undefined ||
+    process.env.REACT_APP_API_BASE_PATH === ''
       ? '/api/v2/neo3'
-      : process.env.API_BASE_PATH,
+      : process.env.REACT_APP_API_BASE_PATH,
 })
