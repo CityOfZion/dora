5d0d6d3b885c6f26d1d7976d18e373ebb8e4b757
Erik van den Brink <git@erikvandenbrink.com>
2026-01-06 14:51:40 +0100

    fix Copy button alignment for Blocks

--- src/components/copy/Copy.tsx
+++ src/components/copy/Copy.tsx
@@ -25,13 +25,13 @@ const Copy: React.FC<CopyProps> = ({ text }) => {
   };
 
   return (
-    <span onClick={copyText} style={{ cursor: 'pointer', display: 'inline-flex', width: 19, justifyContent: 'center' }}>
+    <button onClick={copyText} style={{ cursor: 'pointer', width: 19}}>
       {copied ? (
         <Check style={{ width: 19, color: '#D355E7' }} />
       ) : (
         <FileCopy id="copy-icon" style={{ width: 16, color: '#D355E7' }} />
       )}
-    </span>
+    </button>
   );
 };
 
