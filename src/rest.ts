a1f27ae686dde9b3291df9c5d231f8978ffb2bbb
Erik van den Brink <git@erikvandenbrink.com>
2026-03-26 13:04:03 +0100

    update default `doraURL` of `NeoRest`

--- src/rest.ts
+++ src/rest.ts
@@ -1,6 +1,6 @@
 import { NeoRESTApi } from '@cityofzion/dora-ts/dist/api'
 
 export const NeoRest = new NeoRESTApi({
-  doraUrl: import.meta.env.VITE_API_HOST || 'https://dora.coz.io',
+  doraUrl: import.meta.env.VITE_API_HOST || 'https://api.coz.io',
   endpoint: import.meta.env.VITE_API_BASE_PATH || '/api/v2/neo3',
 })
